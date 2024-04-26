bf7e1066bf7bb59b7a54cf34ecd6e312c0a7f324
Zyperr <ramerica200@gmail.com>
2024-04-25 21:57:18 -0300

    [add] Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
-
+import logo from "../assets/logo.png"
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+        <div className="logo_container">
+          <img className="logo" src={logo} alt="logo" role="img"/>
+          <h1>Chat</h1>
+        </div>
+    </header>
   )
 }
 
